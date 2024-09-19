492653636029e38808c3a722fb5df1ed6b6d62d2
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-09-19 13:39:46 +0530

    adminlogin created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import AddUser from './components/AddUser';
 import LoginUser from './components/LoginUser';
+import LoginAdmin from './components/LoginAdmin';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
+        <Route path='/' element={<LoginAdmin/>}/>
         <Route path='/adduser' element={<AddUser/>}/>
-        <Route path='/' element={<LoginUser/>}/>
+        <Route path='/userlogin' element={<LoginUser/>}/>
       </Routes>
       </BrowserRouter>
       
